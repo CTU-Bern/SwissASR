c20ea13fce2e2af897d78313bb9a596c436595a3
Alan Haynes <aghaynes@users.noreply.github.com>
2022-11-10 07:58:16 +0100

    lots of changes

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,10 @@
-# swissethics 0.0.0.9000
+# swissethics 0.2.0
+
+* Addition of `asr_dataprep` and `asr_safety_summary` functions allowing for production of tables without generation of the MS Word report.
+* Reworking of the internals of `asr` to utilise the above mentioned functions.
+* Addition of demonstration dataset.
+* Line listing pages in the word template set to landscape.
+
+# swissethics 0.1.0
 
 * Added a `NEWS.md` file to track changes to the package.
--- R/checks.R
+++ R/checks.R
@@ -26,3 +26,30 @@ check_log <- function(x){
   var <- sub("data$", "", var, fixed = TRUE)
   if(!any(c("logical") %in% class(x))) stop("'", var, "' should be logical")
 }
+
+
+
+check_listing_names <- function(names
+                                , var_class = "class"
+                                , var_sae_n = "sae_n" #sae ID
+                                , var_part_id = "record_id" #participant ID
+                                , var_age = "age"
+                                , var_sex ="sex"
+                                , var_country = "country"
+                                , var_site = "site"
+                                , var_sae = "sae"
+                                , var_date_onset = "sae_date"
+                                , var_trt = "trt"
+                                , var_date_trt_start = "sae_trtstart"
+                                , var_date_trt_stop = "sae_trtstop"
+                                , var_outcome = "outcome"
+                                , var_comment = "comment"
+                                # , var_relation = "related"
+                                # , var_expected = "expected"
+                                # , var_devdef = "devdef"
+                                # , var_devattr = "devattr"
+                                # , var_devint = "devint"
+                                # , var_safetymeasure = "safetymeasure"
+){
+
+}
